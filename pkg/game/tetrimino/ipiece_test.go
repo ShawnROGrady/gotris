3f24ac404f5cd4eb415d6085dc2342fc8d917482
ShawnROGrady <shawn.r.ogrady@gmail.com>
2019-05-08 22:28:01 -0500

    [tetrimino] added iPiece wall kick verification tests

--- pkg/game/tetrimino/ipiece_test.go
+++ pkg/game/tetrimino/ipiece_test.go
@@ -77,15 +77,75 @@ var iPieceTests = map[orientation]tetriminoTestCase{
 	},
 }
 
+func iPieceWallKickTests() map[orientation]map[orientation][]wallKickTest {
+	tests := make(map[orientation]map[orientation][]wallKickTest)
+	tests[spawn] = map[orientation][]wallKickTest{
+		clockwise: []wallKickTest{
+			{xChange: -2, yChange: 0},
+			{xChange: 1, yChange: 0},
+			{xChange: -2, yChange: -1},
+			{xChange: 1, yChange: 2},
+		},
+		counterclockwise: []wallKickTest{
+			{xChange: -1, yChange: 0},
+			{xChange: 2, yChange: 0},
+			{xChange: -1, yChange: 2},
+			{xChange: 2, yChange: -1},
+		},
+	}
+	tests[clockwise] = map[orientation][]wallKickTest{
+		spawn: []wallKickTest{
+			{xChange: 2, yChange: 0},
+			{xChange: -1, yChange: 0},
+			{xChange: 2, yChange: 1},
+			{xChange: -1, yChange: -2},
+		},
+		opposite: []wallKickTest{
+			{xChange: -1, yChange: 0},
+			{xChange: 2, yChange: 0},
+			{xChange: -1, yChange: 2},
+			{xChange: 2, yChange: -1},
+		},
+	}
+	tests[opposite] = map[orientation][]wallKickTest{
+		clockwise: []wallKickTest{
+			{xChange: 1, yChange: 0},
+			{xChange: -2, yChange: 0},
+			{xChange: 1, yChange: -2},
+			{xChange: -2, yChange: 1},
+		},
+		counterclockwise: []wallKickTest{
+			{xChange: 2, yChange: 0},
+			{xChange: -1, yChange: 0},
+			{xChange: 2, yChange: 1},
+			{xChange: -1, yChange: -2},
+		},
+	}
+	tests[counterclockwise] = map[orientation][]wallKickTest{
+		opposite: []wallKickTest{
+			{xChange: -2, yChange: 0},
+			{xChange: 1, yChange: 0},
+			{xChange: -2, yChange: -1},
+			{xChange: 1, yChange: 2},
+		},
+		spawn: []wallKickTest{
+			{xChange: 1, yChange: 0},
+			{xChange: -2, yChange: 0},
+			{xChange: 1, yChange: -2},
+			{xChange: -2, yChange: 1},
+		},
+	}
+
+	return tests
+}
+
 func TestIPiece(t *testing.T) {
 	piece := newIPiece(4, 4)
 
 	testPiece(t, piece, iPieceTests)
 
 	piece = newIPiece(10, 24)
-	// TODO: define iPiece wallKickTests
-	iPieceWallKickTests := make(map[orientation]map[orientation][]wallKickTest)
-	if err := testRotationTests(piece, iPieceWallKickTests); err != nil {
+	if err := testRotationTests(piece, iPieceWallKickTests()); err != nil {
 		t.Errorf("%s", err)
 	}
 }
