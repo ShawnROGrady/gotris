59936b2b2533193c27bd5f059b18d918c672178f
ShawnROGrady <shawn.r.ogrady@gmail.com>
2019-07-08 21:39:07 -0500

    [board] add tests for blockGridCells

--- internal/game/board/board_test.go
+++ internal/game/board/board_test.go
@@ -355,12 +355,12 @@ func TestCheckRows(t *testing.T) {
 		fullRows := b.CheckRows()
 
 		if len(fullRows) != len(test.expectedFullRows) {
-			t.Fatalf("Unexpected full rows detected for test case '%s' [expected = %v, actual = %v", testName, test.expectedFullRows, fullRows)
+			t.Fatalf("Unexpected full rows detected for test case '%s' [expected = %v, actual = %v]", testName, test.expectedFullRows, fullRows)
 		}
 
 		for i := range fullRows {
 			if fullRows[i] != test.expectedFullRows[i] {
-				t.Fatalf("Unexpected full rows detected for test case '%s' [expected = %v, actual = %v", testName, test.expectedFullRows, fullRows)
+				t.Fatalf("Unexpected full rows detected for test case '%s' [expected = %v, actual = %v]", testName, test.expectedFullRows, fullRows)
 				return
 			}
 		}
@@ -377,7 +377,7 @@ func TestClearFullRows(t *testing.T) {
 
 		clearedRows := b.ClearFullRows()
 		if clearedRows != len(test.expectedFullRows) {
-			t.Fatalf("Unexpected number of cleared rows for test case '%s' [expected = %v, actual = %v", testName, len(test.expectedFullRows), clearedRows)
+			t.Fatalf("Unexpected number of cleared rows for test case '%s' [expected = %v, actual = %v]", testName, len(test.expectedFullRows), clearedRows)
 		}
 
 		if len(b.Blocks) != len(test.initialBlocks) {
@@ -416,8 +416,12 @@ func TestClearFullRows(t *testing.T) {
 		}
 
 		cells := b.Cells()
+		blockGridCells := BlockGridCells(b.Blocks, b.Background(), b.widthScale)
 		if len(cells) != len(test.expectedNewCells) {
 			t.Fatalf("unexpected number of cells for test case '%s' [expected = %d, actual = %d]", testName, len(test.expectedNewCells), len(cells))
+		}
+		if len(blockGridCells) != len(test.expectedNewCells) {
+			t.Fatalf("unexpected number of blockGridCells for test case '%s' [expected = %d, actual = %d]", testName, len(test.expectedNewCells), len(blockGridCells))
 		}
 
 		if len(cells) != len(test.initialBlocks) {
@@ -429,8 +433,12 @@ func TestClearFullRows(t *testing.T) {
 			if len(cells[i]) != len(test.expectedNewCells[i]) {
 				t.Fatalf("unexpected number of cells in updated row %d for test case '%s' [expected = %d, actual = %d]", i, testName, len(test.expectedNewCells[i]), len(cells))
 			}
+			if len(blockGridCells[len(cells)-i-1]) != len(test.expectedNewCells[len(test.expectedNewCells)-i-1]) {
+				t.Fatalf("unexpected number of blockGridCells cells in updated row %d for test case '%s' [expected = %d, actual = %d]", i, testName, len(test.expectedNewCells[len(test.expectedNewCells)-i-1]), len(blockGridCells[len(cells)-i-1]))
+			}
 			for j := range cells[i] {
-				if cells[i][j] == nil && test.expectedNewCells[i][j] == nil {
+				blockGridCellsIndex := len(cells) - i - 1
+				if cells[i][j] == nil && test.expectedNewCells[i][j] == nil && blockGridCells[blockGridCellsIndex][j] == nil && test.expectedNewCells[blockGridCellsIndex][j] == nil {
 					continue
 				}
 
@@ -438,14 +446,26 @@ func TestClearFullRows(t *testing.T) {
 					t.Fatalf("cells[%d][%d] unexpectedly nil for test case '%s'", i, j, testName)
 					return
 				}
+				if blockGridCells[i][j] == nil && test.expectedNewCells[blockGridCellsIndex][j] != nil {
+					t.Fatalf("blockGridCells[%d][%d] unexpectedly nil for test case '%s'", i, j, testName)
+					return
+				}
 
 				if cells[i][j] != nil && test.expectedNewCells[i][j] == nil {
 					t.Fatalf("cells[%d][%d] unexpectedly non-nil for test case '%s'", i, j, testName)
 					return
 				}
+				if blockGridCells[i][j] != nil && test.expectedNewCells[blockGridCellsIndex][j] == nil {
+					t.Fatalf("blockGridCells[%d][%d] unexpectedly non-nil for test case '%s'", i, j, testName)
+					return
+				}
 
 				if cells[i][j].String() != test.expectedNewCells[i][j].String() {
-					t.Fatalf("unexpected new cells[%d][%d] for test case '%s' [expected = %#v, actual = %#v]", i, j, testName, test.expectedNewCells[i][j], cells[i][j])
+					t.Errorf("unexpected new cells[%d][%d] for test case '%s' [expected = %#v, actual = %#v]", i, j, testName, test.expectedNewCells[i][j], cells[i][j])
+					return
+				}
+				if blockGridCells[i][j].String() != test.expectedNewCells[blockGridCellsIndex][j].String() {
+					t.Errorf("unexpected blockGridCells [%d][%d] for test case '%s' [expected = %#v, actual = %#v]", i, j, testName, test.expectedNewCells[blockGridCellsIndex][j], blockGridCells[blockGridCellsIndex][j])
 					return
 				}
 			}
